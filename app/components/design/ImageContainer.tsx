f5b9e79a82e1d4689a9cd063152fa9ecf71b5246
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2024-02-25 22:36:31 -0600

    vercel not hosting image

--- app/components/design/ImageContainer.tsx
+++ app/components/design/ImageContainer.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
 
 export const ImageContainer = ({images}: {images: {id: string, src: string, width: number, height: number, alt: string, description?: string}[]}) => {
     return <div className={`images-collection frames-${images.length}`}>
         {images.map(i => <div key={i.src} className="image-container">
-            <Image src={i.src} width={i.width} height={i.height} alt={i.alt} />
+            {/* <Image src={i.src} width={i.width} height={i.height} alt={i.alt} /> */}
+            <img src={i.src} width={i.width} height={i.height} alt={i.alt} />
             <div>{i.description}</div>
         </div>)}
     </div>
