0f78cbfcd04cdcdbea0a94d601e09786426cb9fb
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2024-02-26 05:18:33 -0600

    whoops

--- app/page.tsx
+++ app/page.tsx
@@ -24,25 +24,25 @@ export default function Home() {
             <li>
               <a href="/work/sleep-number">
                 <span className="sr-only">Sleep Number Work</span>
-                <Image src="/images/sleep-number-work-thumbnail.png" width={600} height={310} alt="" />   
+                <Image src="/static/images/sleep-number-work-thumbnail.png" width={600} height={310} alt="" />   
               </a>
             </li>
             <li>
               <a href="/work/space150">
                 <span className="sr-only">Space150 Website</span>
-                <Image src="/images/space150-website-thumbnail.png" width={600} height={310} alt="" /> 
+                <Image src="/static/images/space150-website-thumbnail.png" width={600} height={310} alt="" /> 
               </a>
             </li>
             <li>
               <a href="/work/music-analytics-app">
                 <span className="sr-only">Music Analytics App</span>
-                <Image src="/images/music-analytics-app-thumbnail.png" width={600} height={310} alt="" />
+                <Image src="/static/images/music-analytics-app-thumbnail.png" width={600} height={310} alt="" />
               </a>
             </li>
             <li>
               <a href="/work/medtronic-project">
                 <span className="sr-only">Medtronic Project</span>
-                <Image src="/images/medtronic-project-thumbnail.png" width={600} height={310} alt="" />
+                <Image src="/static/images/medtronic-project-thumbnail.png" width={600} height={310} alt="" />
               </a>
             </li>
           </ul>
