817918b57e8b698d9bce08c8f74f55290053ff8e
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2024-03-04 07:21:57 -0600

    adding profile to home page

--- app/components/design/ProjectInfo.tsx
+++ app/components/design/ProjectInfo.tsx
@@ -1,21 +1,29 @@
+export type SnapshotsT = {id: string, label: string, content: {id: string, type: 'pill' | 'date', message: string}[]}[]
+
+export interface InventoryI {
+    snapshots: SnapshotsT,
+}
+
+
 export interface ProjectInfoI {
-    snapshots: {id: string, label: string, content: {id: string, type: 'pill' | 'date', message: string}[]}[],
+    snapshots: SnapshotsT,
     blurbs: {id: string, message: string}[]
+}
+
+export const Inventory = ({ snapshots } : InventoryI) => {
+    return <div className="inventory-collection">
+        {snapshots.map(s => <div key={s.id} className={`inventory ${s.id}`}>
+            <div className="inventory-label">{s.label}:</div>
+            <div className="inventory-content">
+                {s.content.map(c => <div key={c.id} className={c.type}>{c.message}</div>)}
+            </div>
+        </div>)}
+    </div>
 }
 
 
 
 export const ProjectInfo = ({ snapshots, blurbs } : ProjectInfoI) => {
-    const Inventory = () => (
-        <div className="inventory-collection">
-            {snapshots.map(s => <div key={s.id} className={`inventory ${s.id}`}>
-                <div className="inventory-label">{s.label}:</div>
-                <div className="inventory-content">
-                    {s.content.map(c => <div key={c.id} className={c.type}>{c.message}</div>)}
-                </div>
-            </div>)}
-        </div>
-    )
      const Blurbs = () => (
         <div className="project-blurb-collection">
             {blurbs.map(b => <p key={b.id}>{b.message}</p>)}
@@ -23,12 +31,12 @@ export const ProjectInfo = ({ snapshots, blurbs } : ProjectInfoI) => {
      )
     return <>
         <div className="project-info desktop">
-            <Inventory />
+            <Inventory snapshots={snapshots}/>
             <Blurbs />
         </div>
         <div className="project-info mobile">
             <Blurbs />
-            <Inventory />
+            <Inventory snapshots={snapshots}/>
         </div>
     </>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import Image from "next/image";
+import { Inventory } from "./components/design/ProjectInfo";
 import { ProjectNav } from "./components/project-navigation/ProjectNav";
 
 export default function Home() {
   return (
     <div>
-      <header>
+      <header id="top">
         <h1>
           <div className="primary-title mb-24">Aaron Miller</div>
           <div className="secondary-title hidden md:block mb-24">A digital designer who creates efficient web experiences, beautiful graphic solutions, and experimental typography.</div>
@@ -20,7 +22,45 @@ export default function Home() {
           </ul>
         </nav> */}
         <ProjectNav ariaLabel="Secondary" />
+        <div className="profile">
+          <div>
+            <Image src='/static/images/aaron-profile-pic.jpg' alt="Aaron Miller" width={1920} height={1080} />
+          </div>
+          <div>
+            <h2 className="secondary-title">Hi, I'm Aaron!</h2>
+            <p>I am a digital designer who loves creating clean user experiences for web experiences while also specializing in the design of graphics, print, and typography.</p>
+            <p>I earned my Bachelor of Graphic Design from the Minneapolis College of Art and Design in 2018 and have worked design internships at HCL Technologies and space150. Most recently I was a Graphic Designer for Sleep Number’s web team.</p>
+            <p>Currently lives in Minneapolis with his wife, two cats, and puppy.</p>
+            <Inventory snapshots={[
+                {id: 'services', label: 'Services', content: [{id: 'graphic-design', type: 'pill', message: 'Graphic Design'}, {id: 'web-design', type: 'pill', message: 'Web Design'}, {id: 'Production Design', type: 'pill', message: 'Production Design'}, {id: 'ux-ui-strategy', type: 'pill', message: 'UX/UI Strategy'}, {id: 'advertising', type: 'pill', message: 'Advertising'}, {id: 'signage', type: 'pill', message: 'Signage'}, {id: 'illustration', type: 'pill', message: 'Illustration'}, {id: 'web-animation', type: 'pill', message: 'Web Animation'}, {id: 'font-design', type: 'pill', message: 'Font Design'}, {id: 'book-layout', type: 'pill', message: 'Book Layout'}]},
+                {id: 'tools', label: 'Tools', content: [{id: 'figma', type: 'pill', message: 'Figma'}, {id: 'sketch', type: 'pill', message: 'Sketch'}, {id: 'photoshop', type: 'pill', message: 'Photoshop'}, {id: 'illustrator', type: 'pill', message: 'Illustrator'}, {id: 'indesign', type: 'pill', message: 'InDesign'}, {id: 'after-effects', type: 'pill', message: 'After Effects'}, {id: 'html', type: 'pill', message: 'HTML'}, {id: 'css', type: 'pill', message: 'CSS'}, {id: 'javascript', type: 'pill', message: 'Javascript'}]},
+              ]}
+            />
+          </div> 
+        </div>
+        <div className="gray-container">
+          <div className="connect-container">
+            <div className="lets-connect">
+              <h2 className="secondary-title">Let&apos;s Connect!</h2>
+              <p>Want to get in touch? Reach me by emailing me directly, or reaching out to me on LinkedIn.</p>
+              <p>Talk soon!</p>
+            </div>
+            <div className="my-links">
+              <h2 className="secondary-title">My Links</h2>
+              <a className="ghostly-button" href="mailto:awmiller1993@gmail.com">awmiller1993@gmail.com</a>
+              <a className="ghostly-button" href="">My Resume</a>
+              <a className="ghostly-button" href="https://www.linkedin.com/in/amiller-design/">LinkedIn</a>
+            </div>
+          </div>
+        </div>
+        <div className="back-to-top-container">
+          <a className="ghostly-button" href="#top">Back to The Top</a>
+        </div>
       </main>
+      <footer className="home-footer">
+        <p>Created in partnership with the lovely <a href="https://www.linkedin.com/in/kiersten-miller-20a292166/">Kiersten Miller</a>. My wife and web developer!</p>
+        <p>version 1.0 _ 2024</p>
+      </footer>
     </div>
   );
 }
