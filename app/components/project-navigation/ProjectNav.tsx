9eabb68d0d42d51860df405080aaf48bf2a5cab1
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2024-03-04 05:09:41 -0600

    filtering out current page in footer nav

--- app/components/project-navigation/ProjectNav.tsx
+++ app/components/project-navigation/ProjectNav.tsx
@@ -1,32 +1,23 @@
-import Image from "next/image"
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export const ProjectNav = ({ariaLabel}: {ariaLabel: string}) => {
+  const pathname = usePathname();
+  const routes = [
+    {href: '/work/sleep-number', src: '/static/images/sleep-number-work-thumbnail.png', srText: 'Sleep Number Work'},
+    {href: '/work/space150', src: '/static/images/space150-website-thumbnail.png', srText: 'Space150 Website'},
+    {href: '/work/music-analytics-app', src: '/static/images/music-analytics-app-thumbnail.png', srText: 'Music Analytics App'},
+    {href: '/work/medtronic-project', src: '/static/images/medtronic-project-thumbnail.png', srText: 'Medtronic Project'}
+  ];
+  const routesToUse = routes.filter(route => route.href !== pathname);
     return <nav aria-label={ariaLabel}>
     <ul className="projects-menu">
-      <li>
-        <a href="/work/sleep-number">
-          <span className="sr-only">Sleep Number Work</span>
-          <Image src="/static/images/sleep-number-work-thumbnail.png" width={600} height={310} alt="" />   
+      {routesToUse.map(route => <li key={route.href}>
+        <a href={route.href}>
+          <span className="sr-only">{route.srText}</span>
+          <Image src={route.src} width={600} height={310} alt="" />
         </a>
-      </li>
-      <li>
-        <a href="/work/space150">
-          <span className="sr-only">Space150 Website</span>
-          <Image src="/static/images/space150-website-thumbnail.png" width={600} height={310} alt="" /> 
-        </a>
-      </li>
-      <li>
-        <a href="/work/music-analytics-app">
-          <span className="sr-only">Music Analytics App</span>
-          <Image src="/static/images/music-analytics-app-thumbnail.png" width={600} height={310} alt="" />
-        </a>
-      </li>
-      <li>
-        <a href="/work/medtronic-project">
-          <span className="sr-only">Medtronic Project</span>
-          <Image src="/static/images/medtronic-project-thumbnail.png" width={600} height={310} alt="" />
-        </a>
-      </li>
+      </li>)}
     </ul>
   </nav>
 }
