cd9b5d14582da753966d330ea7e31ba537094fef
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2024-03-24 15:04:52 -0500

    adding playwright for visual testing

---
+++ tests/on-load-visual-regression-test.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@playwright/test';
+
+test('visual check home page desktop', async ({ page }) => {
+  await page.goto('https://aaronmillerdesign.com/');
+  await expect(page).toHaveScreenshot();
+});
+
+test('visual check sleep number page desktop', async ({ page }) => {
+  await page.goto('https://aaronmillerdesign.com/work/sleep-number');
+  await expect(page).toHaveScreenshot();
+});
+
+test('visual check medtronic page desktop', async ({ page }) => {
+  await page.goto('https://aaronmillerdesign.com/work/medtronic-project');
+  await expect(page).toHaveScreenshot();
+});
+
+// can't take screenshot - playwright takes 2 screenshots in quick succession and the gifs play on page load. Playwright get 2 matching screenshots.
+// test('visual check space150 page desktop', async ({ page }) => {
+//   await page.goto('https://aaronmillerdesign.com/work/space150');
+//   await expect(page).toHaveScreenshot({timeout: 1200000});
+// });
