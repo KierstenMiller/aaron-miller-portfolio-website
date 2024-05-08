064750b43ed7e3259126024f3c04b718484e7f3a
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2024-05-07 21:44:07 -0500

    adding animation to project nav

--- app/components/project-navigation/ProjectNav.tsx
+++ app/components/project-navigation/ProjectNav.tsx
@@ -5,21 +5,23 @@ import { usePathname } from "next/navigation";
 
 import styles from "app/components/project-navigation/ProjectNav.module.scss";
 
-export const ProjectNav = ({ariaLabel}: {ariaLabel: string}) => {
+export const ProjectNav = ({ ariaLabel }: { ariaLabel: string }) => {
   const pathname = usePathname();
   const routes = [
-    {href: '/work/sleep-number', src: '/static/images/sleep-number-work-thumbnail.png', srText: 'Sleep Number Work'},
-    {href: '/work/space150', src: '/static/images/space150-website-thumbnail.png', srText: 'Space150 Website'},
+    { href: '/work/sleep-number', src: '/static/images/sleep-number-work-thumbnail.png', srText: 'Sleep Number Work' },
+    { href: '/work/space150', src: '/static/images/space150-website-thumbnail.png', srText: 'Space150 Website' },
     // {href: '/work/music-analytics-app', src: '/static/images/music-analytics-app-thumbnail.png', srText: 'Music Analytics App'},
-    {href: '/work/medtronic-project', src: '/static/images/Medtronic_Thumb.png', srText: 'Medtronic Project'}
+    { href: '/work/medtronic-project', src: '/static/images/Medtronic_Thumb.png', srText: 'Medtronic Project' }
   ];
   const routesToUse = routes.filter(route => route.href !== pathname);
-    return <nav aria-label={ariaLabel}>
+  return <nav aria-label={ariaLabel}>
     <ul className={styles['projects-menu']}>
       {routesToUse.map(route => <li key={route.href}>
         <a href={route.href}>
-          <span className="sr-only">{route.srText}</span>
-          <Image src={route.src} width={600} height={310} alt="" />
+          <div className={styles['stuff-container']}>
+            <Image src={route.src} width={600} height={310} alt="" />
+            <span className={styles['project-name']}>{route.srText}</span>
+          </div>
         </a>
       </li>)}
     </ul>
