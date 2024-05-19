745cbecafa690baf269f64c583994e9d4b01d5f1
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2024-05-19 17:21:35 -0500

    updating version

--- app/components/project-page-layout/ProjectPageLayout.tsx
+++ app/components/project-page-layout/ProjectPageLayout.tsx
@@ -5,7 +5,7 @@ import styles from "./ProjectPageLayout.module.scss";
 
 export const fullWidth = 1240;
 
-export const ProductPageLayout = ({primaryTitle, projectInfo, children}:{ primaryTitle: string, projectInfo: ProjectInfoI, children: React.ReactNode;}) => {
+export const ProductPageLayout = ({ primaryTitle, projectInfo, children }: { primaryTitle: string, projectInfo: ProjectInfoI, children: React.ReactNode; }) => {
     return (<div>
         <header id="top">
             <div className={styles['page-heading-container']}>
@@ -14,22 +14,22 @@ export const ProductPageLayout = ({primaryTitle, projectInfo, children}:{ primar
                     <a className={styles['x-home-button']} href="/">
                         <span className="sr-only">Back to Home</span>
                         <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="20" cy="20" r="19.625" fill="#fff" opacity={0.7}/>
-                            <circle cx="20" cy="20" r="19.625" stroke="#636363" strokeWidth="0.75"/>
-                            <path d="M10.5884 11.1758L28.5296 29.117" stroke="#636363" strokeWidth="0.75" strokeLinejoin="round"/>
-                            <path d="M28.824 11.1758L10.8828 29.117" stroke="#636363" strokeWidth="0.75" strokeLinejoin="round"/>
+                            <circle cx="20" cy="20" r="19.625" fill="#fff" opacity={0.7} />
+                            <circle cx="20" cy="20" r="19.625" stroke="#636363" strokeWidth="0.75" />
+                            <path d="M10.5884 11.1758L28.5296 29.117" stroke="#636363" strokeWidth="0.75" strokeLinejoin="round" />
+                            <path d="M28.824 11.1758L10.8828 29.117" stroke="#636363" strokeWidth="0.75" strokeLinejoin="round" />
                         </svg>
                     </a>
                 </div>
-                
+
             </div>
         </header>
-        <main> 
+        <main>
             <ProjectInfo
                 snapshots={projectInfo.snapshots}
                 blurbs={projectInfo.blurbs}
             />
-            { children }
+            {children}
         </main>
         <footer className={styles['footer-container']}>
             <div className={styles['footer']}>
@@ -40,9 +40,10 @@ export const ProductPageLayout = ({primaryTitle, projectInfo, children}:{ primar
                 <ProjectNav ariaLabel="Primary" />
                 <div className={styles['partnership']}>
                     <p>Created in partnership with the lovely <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kiersten-miller-20a292166/">Kiersten Miller</a>. My wife and web developer!</p>
-                    <p>version 1.0 _ 2024</p>
+                    <p>version 2.0 _ 2024</p>
                 </div>
-            </div>   
+            </div>
         </footer>
     </div>
-)}
+    )
+}
