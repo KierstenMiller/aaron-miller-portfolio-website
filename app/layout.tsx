f2de155550b2e06df10b889735b2f504d5394fdc
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2024-07-01 05:13:41 -0500

    trying nextjs metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,23 @@ import "./globals.scss";
 export const metadata: Metadata = {
   title: "Aaron Miller Design",
   description: "A digital designer who creates efficient web experiences, beautiful graphic solutions, and experimental typography.",
+  metadataBase: new URL('https://aaronmillerdesign.com'),
+  openGraph: {
+    title: 'Aaron Miller Design',
+    description: 'A digital designer who creates efficient web experiences, beautiful graphic solutions, and experimental typography.',
+    url: 'https://aaronmillerdesign.com',
+    siteName: 'Aaron Miller Design',
+    images: [
+      {
+        url: '/static/images/aaron-profile-pic_mobile.png', // Must be an absolute URL
+        width: 681,
+        height: 681,
+        alt: 'Aaron Miller headshot',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
